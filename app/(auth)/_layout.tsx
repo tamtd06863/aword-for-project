dc814d33c7f2de49db81f6c32477f3e16224b5a8
Bodu <tn212079@gmail.com>
2025-09-26 05:34:38 +0700

    fix(auth): unmount onAuthStateChange

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -7,37 +7,43 @@ import { clearAuth, setAuth } from "@/lib/features/auth/authSlice";
 const MainLayout = () => {
   const dispatch = useAppDispatch();
 
-  const { data } = supabase.auth.onAuthStateChange((event, session) => {
-    if (event === "INITIAL_SESSION") {
-      // handle initial session
-      if (session && session.user && session.user.email) {
-        dispatch(
-          setAuth({
-            userId: session.user.id,
-            email: session.user.email,
-            name: session.user.user_metadata.full_name,
-            avatar_url: session.user.user_metadata.avatar_url,
-          }),
-        );
+  React.useEffect(() => {
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === "INITIAL_SESSION") {
+        // handle initial session
+        if (session && session.user && session.user.email) {
+          dispatch(
+            setAuth({
+              userId: session.user.id,
+              email: session.user.email,
+              name: session.user.user_metadata.full_name,
+              avatar_url: session.user.user_metadata.avatar_url,
+            }),
+          );
+        }
+      } else if (event === "SIGNED_IN") {
+        if (session && session.user && session.user.email) {
+          dispatch(
+            setAuth({
+              userId: session.user.id,
+              email: session.user.email,
+              name: session.user.user_metadata.full_name,
+              avatar_url: session.user.user_metadata.avatar_url,
+            }),
+          );
+        }
+        // handle sign in event
+        console.log(event, session);
+      } else if (event === "SIGNED_OUT") {
+        // handle sign out event
+        dispatch(clearAuth());
       }
-    } else if (event === "SIGNED_IN") {
-      if (session && session.user && session.user.email) {
-        dispatch(
-          setAuth({
-            userId: session.user.id,
-            email: session.user.email,
-            name: session.user.user_metadata.full_name,
-            avatar_url: session.user.user_metadata.avatar_url,
-          }),
-        );
-      }
-      // handle sign in event
-      console.log(event, session);
-    } else if (event === "SIGNED_OUT") {
-      // handle sign out event
-      dispatch(clearAuth());
-    }
-  });
+    });
+
+    return () => {
+      data.subscription.unsubscribe();
+    };
+  }, []);
 
   return (
     <Stack>
